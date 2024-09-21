f3585b9a36b61d2bcdca44f4509cd85c2678a75d
AchRyanAdianto <achadianto2007@gmail.com>
2024-09-21 22:10:52 +0700

    php

---
+++ websekolah/content.php
@@ -0,0 +1,9 @@
+<?php
+
+$sekolah = "SMKN 2 buduran";
+$judul = "SMKN 2 buduran";
+$isi = "ini adalah web yang saya buat pada tanggal 15 tahun 2024";
+$hari = ["senin","selasa","rabu","kamis","jumat"];
+$mapel = ["Rpl","Sejarah","agama","bhs indo","bjerman","bhs jepang","pkn","pkwu","binggris","penjas","bjawa","mtk"];
+$gambar =["city.jpg","3d.jpg","mp.jpg"];
+?>
