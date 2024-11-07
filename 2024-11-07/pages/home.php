a85c43fdddd3a806b1921e8179519b80e8c4202e
AchRyanAdianto <achadianto2007@gmail.com>
2024-11-07 09:37:47 +0700

    Add files via upload

---
+++ 2024-11-07/pages/home.php
@@ -0,0 +1,4 @@
+<div class="home">
+    <h2>home</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quasi ad iusto.</p>
+</div>
---
+++ 2024-11-07/pages/kontak.php
@@ -0,0 +1,4 @@
+<div class="kontak">
+    <h2>Kontak</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus exercitationem libero!</p>
+</div>
